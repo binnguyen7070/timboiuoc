253475ee36182db422e9ce3931a8e5bc843d8abc
binnguyen7070 <ngthanhtung0504@gmail>
2024-06-24 19:51:11 +0700

    Add project files.

---
+++ bài tập 2/bài tập 2.cpp
@@ -0,0 +1,38 @@
+﻿// bài tập 2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  /*
+  int soUoc;
+  cout << "Nhap so tim uoc!!!";
+  cout << "\nNhap so:";
+  cin >> soUoc;
+  cout << " Uoc cua " << soUoc << " la :\n";
+  for ( int i = 1; i <= soUoc; i++ )
+{
+   if ( soUoc % i == 0 )
+   {
+   cout << i << " ";
+   }
+ } 
+ cout << endl << endl << endl;
+ */ 
+	int soBoi, SoCuoi, KetQua = 1;
+	cout << "Nhap 2 so tim Boi!!!";
+	cout << "\Nhap so:";
+	cin >> soBoi;
+	cout << "\nSo ket thuc :";
+	cin >> SoCuoi;
+	int i = 1;
+
+	while (KetQua < SoCuoi)
+	{
+		KetQua = soBoi * i;
+		i++;
+		cout << KetQua << " ";
+	}
+
+	return 0; 
